865273c65809ced858e0880a658b40a0712c22a8
Rayhan Islam Shagor <raihanislamshagor2@gmail.com>
2024-07-05 15:36:37 +0600

    push notification on air resolve

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,33 @@
-"use client";
+'use client';
 
-import { Button } from "@/components/ui/button";
-import useFcmToken from "@/hooks/useFcmToken";
+import { Button } from '@/components/ui/button';
+import useFcmToken from '@/hooks/useFcmToken';
 
 export default function Home() {
   const { token, notificationPermissionStatus } = useFcmToken();
 
   const handleTestNotification = async () => {
-    const response = await fetch("/send-notification", {
-      method: "POST",
+    const response = await fetch('/send-notification', {
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify({
         token: token,
-        title: "Test Notification",
-        message: "This is a test notification",
-        link: "/contact",
+        title: 'Test Notification',
+        message: 'This is a test notification',
+        link: '/contact',
       }),
     });
 
-    const data = await response.json();
-    console.log(data);
+    // const data = await response.json();
   };
 
   return (
     <main className="p-10">
       <h1 className="text-4xl mb-4 font-bold">Firebase Cloud Messaging Demo</h1>
 
-      {notificationPermissionStatus === "granted" ? (
+      {notificationPermissionStatus === 'granted' ? (
         <p>Permission to receive notifications has been granted.</p>
       ) : notificationPermissionStatus !== null ? (
         <p>
